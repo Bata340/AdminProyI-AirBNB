7f30b72ef63eedd0fb45162b104142f665774768
agustinaa235 <asegura@fi.uba.ar>
2022-10-15 16:42:15 -0300

    Refactor: in login and register

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.appRoutes import router
 
+
 app = FastAPI()
 app.include_router(router)
 
--- BackEnd/routes/appRoutes.py
+++ BackEnd/routes/appRoutes.py
@@ -1,31 +1,28 @@
 from fastapi import APIRouter
 from fastapi import APIRouter, Depends, HTTPException, status, Request
-from models.models import User
+from model import schema
 import json
 
 router = APIRouter()
 registeredUsers = {}
 
 @router.post("/register", status_code=status.HTTP_200_OK)
-async def register(req: Request):
+async def register(user: schema.User):
 
-    user = json.loads(await req.body())
-
-    if user['username'] in registeredUsers.keys():
+    if user.username in registeredUsers.keys():
         return HTTPException(status_code=500, detail="A user with name " + user["username"] + " already exists")
 
-    registeredUsers[user['username']] = user["password"]
+    registeredUsers[user.username] = user.password
     return {"message" : "registered user"}
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-async def login(req: Request):
+async def login(user: schema.User):
 
-    user = json.loads(await req.body())
+    if user.username not in registeredUsers.keys():
+        return HTTPException(status_code=404, detail="User with name " + user["username"] + " does not exist")
 
-    if user['username'] not in registeredUsers.keys():
-        return HTTPException(status_code=500, detail="User with name " + user["username"] + " does not exist")
-
-    registeredUsers[user['username']] = user["password"]
+    if registeredUsers[user.username] != user.password:
+        return HTTPException(status_code=401, detail="Wrong password")
     return {"message" : "ok"}
 
