b1b808b57f7abe6a7f3776cb3a3c6f338774067c
agustinaa235 <asegura@fi.uba.ar>
2022-10-15 17:43:01 -0300

    Feat: attributes users and get user

--- BackEnd/model/schema.py
+++ BackEnd/model/schema.py
@@ -1,7 +1,18 @@
+from typing import Union
 from pydantic import BaseModel
+from tomlkit import integer
 
 
 class User(BaseModel):
     username: str
     password: str
-    
+    first_name: str
+    last_name: str
+    age: Union[int, None] = None
+    phone_number : Union[str, None] = None
+    location: str
+    login: bool
+
+class UserLogin(BaseModel):
+    username: str
+    password: str    
--- BackEnd/routes/appRoutes.py
+++ BackEnd/routes/appRoutes.py
@@ -11,18 +11,23 @@ async def register(user: schema.User):
 
     if user.username in registeredUsers.keys():
         return HTTPException(status_code=500, detail="A user with name " + user["username"] + " already exists")
-
-    registeredUsers[user.username] = user.password
-    return {"message" : "registered user"}
+    user.login = False
+    registeredUsers[user.username] = user
+    return {"message" : "registered user " +  user.username}
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-async def login(user: schema.User):
+async def login(user: schema.UserLogin):
 
     if user.username not in registeredUsers.keys():
         return HTTPException(status_code=404, detail="User with name " + user.username + " does not exist")
-
-    if registeredUsers[user.username] != user.password:
+    if registeredUsers[user.username].password != user.password:
         return HTTPException(status_code=401, detail="Wrong password")
+    registeredUsers[user.username].login = True
     return {"message" : "ok"}
 
+@router.get("/{username}", status_code=status.HTTP_200_OK)
+async def get(username: str):
+    if username not in registeredUsers.keys():
+        return HTTPException(status_code=404, detail="User with name " + username + " does not exist")
+    return {"message": registeredUsers[username]}
